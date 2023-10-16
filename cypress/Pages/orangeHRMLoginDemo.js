93e45ffa8cbf286c152eebbe12534d750ffd8405
dineshsre <dineshsre@gmail.com>
2023-10-16 17:00:48 +0530

    corrected  oraneghrmdemo  for SLACK Integration for html reports

--- cypress/Pages/orangeHRMLoginDemo.js
+++ cypress/Pages/orangeHRMLoginDemo.js
@@ -2,7 +2,7 @@ export class orangeHRM{
 
     OrangeHRMLocators={
         userName: "input[placeholder='Username']",
-        password: "input[placeholder='Passsword']",
+        password: "input[placeholder='Password']",
         login: "button[type='submit']"
     }
 
